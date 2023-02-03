4713a292b2be6c5b6c3b87ac19c391c1b606adf5
Jason Tully <jtully@installationmadeeasy.com>
2023-02-03 13:08:30 -0500

    added more examples.

---
+++ examples/net6+/ExamplesCommon/WeatherForecastServices.cs
@@ -0,0 +1,51 @@
+﻿using Dependous;
+using Dependous.Autofac;
+using ExamplesCommon;
+
+namespace ConsoleExample
+{
+    [Target(typeof(IWeatherService)), NamedDependency(DependencyNames.Accuweather)]
+    public class AccuweatherService : IWeatherService, ISingletonDependency
+    {
+        public Task<string> GetForecastAsync(string location)
+        {
+            return Task.FromResult($"The forecase for: {location} is Sunny 58 degrees. Forcast provider by {DependencyNames.Accuweather}");
+        }
+    }
+
+    [Target(typeof(IWeatherService)), NamedDependency(DependencyNames.WeatherChannel)]
+    public class WeatherChannelService : IWeatherService, ISingletonDependency
+    {
+        public Task<string> GetForecastAsync(string location)
+        {
+            return Task.FromResult($"The forecast for: {location} is Raining 47 degrees.  Forcast provider by {DependencyNames.WeatherChannel}");
+        }
+    }
+
+
+    public class PostalCodeService : IPostalCodeService, ISingletonDependency
+    {
+        public bool IsValidAsync(string code)
+        {
+            return true;
+        }
+    }
+
+    public class MethodCallInterceptorDecorator : IPostalCodeService, IDecorator<IPostalCodeService>, ISingletonDependency
+    {
+        private readonly IPostalCodeService trueService;
+
+        public MethodCallInterceptorDecorator(IPostalCodeService trueService)
+        {
+            this.trueService = trueService;
+        }
+
+        public bool IsValidAsync(string code)
+        {
+            Console.WriteLine("Before GetForecastAsync");
+            var result = this.trueService.IsValidAsync(code);
+            Console.WriteLine("After GetForecastAsync");
+            return result;
+        }
+    }
+}
--- examples/net6+/MinimalWebApi/Controllers/WeatherForecastController.cs
+++ examples/net6+/MinimalWebApi/Controllers/WeatherForecastController.cs
@@ -1,3 +1,5 @@
+using System.Collections.Concurrent;
+using Autofac.Features.Indexed;
 using ExamplesCommon;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,28 +15,54 @@ public class WeatherForecastController : ControllerBase
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
+    private readonly IEnumerable<IWeatherService> weatherServices;
+    private readonly IIndex<DependencyNames, IWeatherService> serviceMap;
+    private readonly IPostalCodeService postalCodeService;
     private readonly IMyService myService;
     private readonly MySingleton mySingleton;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger, IMyService myService, MySingleton mySingleton)
+    public WeatherForecastController(ILogger<WeatherForecastController> logger, IEnumerable<IWeatherService> weatherServices,
+        IIndex<DependencyNames, IWeatherService> serviceMap,
+        IPostalCodeService postalCodeService)
     {
         _logger = logger;
+        this.weatherServices = weatherServices;
+        this.serviceMap = serviceMap;
+        this.postalCodeService = postalCodeService;
         this.myService = myService;
         this.mySingleton = mySingleton;
     }
 
+    /// <summary>
+    /// In this example, we show how we can invoke all implementations of IWeatherService.
+    /// </summary>
+    /// <returns></returns>
     [HttpGet(Name = "GetWeatherForecast")]
-    public async Task<IEnumerable<WeatherForecast>> Get()
+    public async Task<IEnumerable<string>> Get()
     {
-        var message = await this.myService.ExecuteAsync();
-        var debug = this.mySingleton.ScanResults.ToString();
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        var isValid = this.postalCodeService.IsValidAsync("30030");
+
+        var state = new ConcurrentQueue<string>();
+        await Parallel.ForEachAsync(weatherServices, async (item, token) =>
         {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)],
-            Message = $"{message} {debug}"
-        })
-        .ToArray();
+            var forecast = await item.GetForecastAsync("Atlanta");
+            state.Enqueue(forecast);
+        });
+        return state.ToList();
+    }
+
+    /// <summary>
+    /// In this example, we show how to target a specific "named" dependency by a key.  Keys can by an object type.  I prefer to use strings or enums.
+    /// </summary>
+    /// <param name="dependencyNames">The dependency names.</param>
+    /// <returns></returns>
+    [HttpGet("byProvider")]
+    public async Task<string> Get([FromQuery] DependencyNames dependencyNames)
+    {
+        if (serviceMap.TryGetValue(dependencyNames, out var service))
+        {
+            return await service.GetForecastAsync("Atlanta");
+        }
+        return null;
     }
 }
